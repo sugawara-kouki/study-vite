d0f0941c518718de981caf1342621ef827dd0d98
sugawara-kouki <kou.720231@gmail.com>
2024-01-18 23:40:32 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      newTodoText: '',
+      todos: [
+        // { isDone: false, text: 'ToDoの文字列'}
+      ],
+    }
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodoText) return alert('文字を入力してください')
+      this.todos.push({
+        isDone: false,
+        text: this.newTodoText,
+      })
+      this.newTodoText = ''
+    },
+    clearDoneTodos() {
+      this.todos = this.todos.filter((todo) => !todo.isDone)
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>My ToDo App</h1>
+  <input type="text" v-model="newTodoText" /><button @click="addTodo">
+    追加</button><button @click="clearDoneTodos">完了済みを削除する</button>
+  <p v-if="todos.length === 0">ToDoがまだありません！</p>
+  <ul v-else>
+    <li v-for="todo in todos">
+      <input type="checkbox" v-model="todo.isDone" /><span :class="{ 'todo-done': todo.isDone }">{{ todo.text }}</span>
+    </li>
+  </ul>
+</template>
+
+<style>
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
+}
+</style>
